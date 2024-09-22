6d971619a31724a313236ee8e4d7f39326a1f4a4
enesGithubUser <enes_gkaya@hotmail.com>
2024-09-22 17:40:12 +0300

    Add files via upload
    scripts added to project.

---
+++ scripts.sql
@@ -0,0 +1,7 @@
+INSERT INTO customer VALUES (1, 1000, 'mail1', 'user1');
+INSERT INTO customer VALUES (2, 2000, 'mail2', 'user2');
+INSERT INTO customer VALUES (3, 3000, 'mail2', 'user2');
+
+INSERT INTO asset VALUES (1, 'TRY', 1, 1, 1);
+INSERT INTO asset VALUES (2, 'TRY', 2, 1, 1);
+INSERT INTO asset VALUES (3, 'TRY', 3, 1, 1);
